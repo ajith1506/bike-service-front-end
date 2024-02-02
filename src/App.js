137ac86b13c1e1f630ee5a33b82e1b9f64e9b809
ajith1506 <ajithkumararun1111@gmail.com>
2024-02-02 10:58:26 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./components/Home/Home";
+import Customer from "./components/Customer/Customer";
+import Admin from "./components/Member/Admin/Admin";
+import Mechanic from "./components/Member/Mechanic/Mechanic";
+import Login from "./components/Home/Login";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Home />
+      <Routes>
+        {/* <Route exact path="/" component={Home} /> */}
+        <Route path="/cust_home" component={Customer} />
+        <Route path="/admin_home" component={Admin} />
+        <Route path="/mechanic_home" component={Mechanic} />
+        <Route exact path="/login" component={Login} />
+      </Routes>
+    </Router>
   );
 }
 
